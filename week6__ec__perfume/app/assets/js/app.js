c021ba706cdcb4029f4e5965063f468e348e5c0e
hsuifang <hsuifang080318@gmail.com>
2020-12-01 21:54:47 +0800

    feat: 新增香水電商v1

---
+++ week6__ec__perfume/app/assets/js/app.js
@@ -0,0 +1,27 @@
+$(function() {
+  // toggle menu
+  $('#js-mobileMenuToggle').on('click', function() { 
+    $('#mobileMenu').toggle('slow')
+    $('.navigation__toggle').toggleClass('navigation__toggle--close')
+  })
+
+  // Modal
+  //
+  $('.js-wishlist').on('click', function(e) {
+    e.preventDefault()
+    alertInfoModal('已加入我的收藏')
+  })
+
+  $('.js-addCart').on('click', function(e) {
+    e.preventDefault()
+    alertInfoModal('已加入購物車')
+  })
+  // 顯示彈跳視窗 - alert
+  function alertInfoModal(alertText = '') {
+    $('#alertInfoModal').modal('show')
+    document.getElementById("alertText").innerText = alertText
+    setTimeout(function() { $('#alertInfoModal').modal('hide') }, 1500)
+  }
+  
+});
+
---
+++ week6__ec__perfume/dist/assets/js/app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+$(function () {
+  // toggle menu
+  $('#js-mobileMenuToggle').on('click', function () {
+    $('#mobileMenu').toggle('slow');
+    $('.navigation__toggle').toggleClass('navigation__toggle--close');
+  }); // Modal
+  //
+
+  $('.js-wishlist').on('click', function (e) {
+    e.preventDefault();
+    alertInfoModal('已加入我的收藏');
+  });
+  $('.js-addCart').on('click', function (e) {
+    e.preventDefault();
+    alertInfoModal('已加入購物車');
+  }); // 顯示彈跳視窗 - alert
+
+  function alertInfoModal() {
+    var alertText = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+    $('#alertInfoModal').modal('show');
+    document.getElementById("alertText").innerText = alertText;
+    setTimeout(function () {
+      $('#alertInfoModal').modal('hide');
+    }, 1500);
+  }
+});
+//# sourceMappingURL=app.js.map
