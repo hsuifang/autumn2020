e78508f9015577c149e72db2d71d613bf24a9ac1
hsuifang <hsuifang080318@gmail.com>
2020-12-27 20:03:25 +0800

    feat: 新增 login / reservation / searchresult / detail / bookingInfo 頁面

--- week8__ec__hotel/app/assets/js/app.js
+++ week8__ec__hotel/app/assets/js/app.js
@@ -1,6 +1,10 @@
 $(function() {
   // index
   // --------------
+  // carousel
+  $('.carousel').carousel({
+    interval: 10000
+  })
   // 選擇時間
   $('#js-datepicker').daterangepicker({
     "autoApply": true
@@ -8,4 +12,17 @@ $(function() {
     document.getElementById("js-datepicker-content").innerText = `${start.format('YYYY-MM-DD')} - ${end.format('YYYY-MM-DD') }`
   });
   // index end --
+
+  // reservationDetail
+  $('#collapseRoomDetail').on('show.bs.collapse', function () {
+    $('.reservationDetail-collapse__toggle .material-icons').text('keyboard_arrow_up')
+    $('.js-detailPrize').css('color', 'transparent')
+    $('.js-detailPrize span').css('display', 'none')
+  })
+  $('#collapseRoomDetail').on('hide.bs.collapse', function () {
+    $('.reservationDetail-collapse__toggle .material-icons').text('keyboard_arrow_down')
+    $('.js-detailPrize').css('display', 'block')
+    $('.js-detailPrize').css('color', '#000')
+    $('.js-detailPrize span').css('display', 'inline')
+  })
 });
